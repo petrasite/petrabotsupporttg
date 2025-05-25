d37aad6c9924237aa150ea732722e643b202b75d
petrasite <daurfedor@outlook.com>
2025-05-25 20:20:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 const BOT_TOKEN = '7856283741:AAHWGZz4fk0F2AU7auwsv82p_xeOtr7N9LM';
-const ADMIN_ID = 'Petradoshikkakw';
+const ADMIN_ID = '7322138308';
 
 const bot = new TelegramBot(BOT_TOKEN, { polling: true });
 
